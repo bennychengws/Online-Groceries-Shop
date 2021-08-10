288bc0d5bbbdcbb475549bd1499dd5a2e7d03197
bennychengws <cwshing1@gmail.com>
2021-08-10 23:59:03 +0800

    Product interactive frontend commited

--- pages/product.jsx
+++ pages/product.jsx
@@ -3,7 +3,9 @@ import Router from 'next/router'
 import moduleCss from "../styles/product.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import Rating from "../components/Rating"
 import heartE from "../images/heartEmpty.png";
+import heartR from "../images/heartRed.png"
 import downArrow from "../images/downArrow.png";
 import expandArrow from "../images/back arrow.png";
 import backArrow from "../images/back_arrow.png";
@@ -21,10 +23,13 @@ const product = () => {
     productDetail:
       "Apples are nutritious. Apples may be good for weight loss. Apples may be good for your heart. As kind of a healthful and varied diet.",
     nutritions: "100g",
-    rating: 5,
+    rating: 4.3,
   };
 
   const [showModal, setShowModal] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isFavourite, setIsFavourite] = useState(false);
+
 
   return (
     <div className={moduleCss.container}>
@@ -33,12 +38,15 @@ const product = () => {
         <div className={moduleCss.productImage}>{product.productImage}</div>
       </div>
       <div>
-        <div className={moduleCss.nameAndHeart}><div>{product.name}</div><div><Image src={heartE} width="24px" height="24px"></Image></div></div>
+        <div className={moduleCss.nameAndHeart}><div>{product.name}</div><div className={moduleCss.heartImg} onClick={() => setIsFavourite(!isFavourite)}><Image src={isFavourite ? heartR: heartE} layout="fill" objectFit="contain" quality={100}></Image></div></div>
         <div className={moduleCss.amount}>{product.amount}</div>  
         <div className={moduleCss.qtyAndPrice}><div className={moduleCss.qtyPanel}><div className={moduleCss.reduceQtyButton}><Image src={reduce} width="35px" height="35px"></Image></div><div>{product.quantity}</div><div className={moduleCss.addQtyButton}><Image src={add} width="35px" height="35px"></Image></div></div><div>${product.price}</div></div>
-        <div className={moduleCss.productDetailPanel}><div className={moduleCss.productDetailAndExpand}><div className={moduleCss.infoTitle}>Product Detail</div><div className={moduleCss.expandButton}><Image src={expandArrow} width="8.4px" height="14px"></Image></div></div><div className={moduleCss.productDetail}>{product.productDetail}</div></div>
+        <div className={moduleCss.productDetailPanel}><div className={moduleCss.productDetailAndExpand} onClick={() => setIsCollapsed(!isCollapsed)}><div className={moduleCss.infoTitle}>Product Detail</div><div className={moduleCss.expandButton}>{isCollapsed ? <Image src={downArrow} width="14px" height="8.4px"></Image> : <Image src={expandArrow} width="8.4px" height="14px"></Image>}</div></div><div className={moduleCss.productDetail} style={{display: isCollapsed ? "block" : "none"}}>{product.productDetail}</div></div>
         <div className={moduleCss.infoPanel}><div className={moduleCss.infoTitle}>Nutritions</div><div className={moduleCss.nutri}>{product.nutritions}</div></div>
-        <div className={moduleCss.infoPanel}><div className={moduleCss.infoTitle}>Review</div><div>{product.rating}</div></div>
+        <div className={moduleCss.infoPanel}>
+          <div className={moduleCss.infoTitle}>Review</div>
+          <div><Rating>{product.rating}</Rating></div>
+        </div>
       </div> 
       <Link href="../cart">
           <button className={moduleCss.addToBasket} onClick={() => setShowModal(true)}>Add To Basket</button>
