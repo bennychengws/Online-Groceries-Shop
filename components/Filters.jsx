e12a54f5fc9570142ed36282475ee6ed8ccb294a
bennychengws <cwshing1@gmail.com>
2021-11-12 18:47:26 +0800

    search filter in progress

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -3,28 +3,57 @@ import ReactDOM from "react-dom";
 import moduleCss from "../styles/Filters.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { set } from "mongoose";
 
-const Filters = ({ show, onClose }) => {
+const Filters = ({ show, onClose, categoryData, brandData }) => {
 
-  const catList = [
-    "Eggs", "Noodles & Pasta", "Chips & Crisps", "Fast Fodd"
-  ];
+  const [newCatList, setNewCatList] = useState([]);
+  const [newbrandList, setNewBrandList] = useState([]);
 
-  const brandList = [
-    "Individual Collection", "Cocola", "Ifad", "Kazi Farmas"
-  ];
+  const [catCheckedState, setCatCheckedState] = useState(
+    new Array(categoryData.length).fill(false)
+  );
+
+  const [brandCheckedState, setBrandCheckedState] = useState(
+    new Array(brandData.length).fill(false)
+  );
+  // const catList = [
+  //   "Eggs", "Noodles & Pasta", "Chips & Crisps", "Fast Food"
+  // ];
+
+  // const brandList = [
+  //   "Individual Collection", "Cocola", "Ifad", "Kazi Farmas"
+  // ];
 
   const [isBrowser, setIsBrowser] = useState(false);
 
   useEffect(() => {
     setIsBrowser(true);
+    // setCatList(categoryData);
+    // setBrandList(brandData);
     console.log("filter created");
   }, []);
+  
+  const handleOnChange = (position, targetField, setTargetField) => {
+    const updatedCheckedState = targetField.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setTargetField(updatedCheckedState)
+  };
 
   const handleCloseClick = (e) => {
     e.preventDefault();
     onClose();
   };
+
+  const handleApplyFilter = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  console.log(catCheckedState)
+  console.log(brandCheckedState)
 
   const modalContent = show ? (
     <div className={moduleCss.container}>
@@ -35,25 +64,28 @@ const Filters = ({ show, onClose }) => {
       </div>
       <div className={moduleCss.filterPanel}>
         <div className={moduleCss.filterContent}>
-          <div className={moduleCss.Title}>Categories</div>
-          <div class="mt-4 flex flex-col">
-            {catList.map((category) => (
-              <label className="inline-flex items-center mb-2">
-                <input type="checkbox" className="form-checkbox text-green-500 rounded" defaultChecked></input>
+          {categoryData.length === 0 ? <div></div> : <div className={moduleCss.Title}>Categories</div>}
+          <div className="mt-4 flex flex-col">
+            {categoryData.map((category, index) => (
+              <label className="inline-flex items-center mb-2" key={index}>
+                <input type="checkbox" className="form-checkbox text-green-500 rounded" name={category} value={category} onChange={() => handleOnChange(index, catCheckedState, setCatCheckedState)} defaultChecked></input>
                 <span className={`ml-2 ${moduleCss.optionColor}`}>{category}</span>
               </label>              
             ))}
           </div>
-          <div className={moduleCss.Title}>Brand</div>
-          <div class="mt-4 flex flex-col">
-            {brandList.map((brand) => (
-              <label className="inline-flex items-center mb-2">
-                <input type="checkbox" className="form-checkbox text-green-500 rounded" defaultChecked></input>
+          {brandData.length === 0 ? <div></div> : <div className={moduleCss.Title}>Brand</div>}
+          <div className="mt-4 flex flex-col">
+            {brandData.map((brand, index) => (
+              <label className="inline-flex items-center mb-2" key={index}>
+                <input type="checkbox" className="form-checkbox text-green-500 rounded" name={brand} value={brand} defaultChecked></input>
                 <span className={`ml-2 ${moduleCss.optionColor}`}>{brand}</span>
               </label>              
             ))}
           </div>
         </div>
+        {categoryData.length === 0 && brandData.length === 0 ? <div></div> :
+          <button className={moduleCss.applyFilter} onClick={handleApplyFilter}>Apply Filter</button>
+        }
       </div>
     </div>
   ) : null;
--- pages/search/[slug].jsx
+++ pages/search/[slug].jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 // import { useRouter } from 'next/router'
 import NavBar from "../../components/NavBar";
 import backArrow from "../../images/back_arrow.png";
+import authenticationCheck from "../../lib/authenticationCheck";
 import filterIcon from "../../images/Filter.png";
 import Filters from "../../components/Filters";
 import SearchBox from "../../components/SearchBox";
@@ -30,46 +31,88 @@ const search = (props) => {
   const {data} = props
 //   console.log("passed data: " + data)
   const [showFilter, setShowFilter] = useState(false);
-  const beverageList = [
-    {
-      name: "Diet Coke",
-      productImage: <Image src={dietCoke} width="35px" height="80px"></Image>,
-      amount: "355ml",
-      price: 7,
-    },
-    {
-      name: "Sprite Can",
-      productImage: <Image src={sprite} width="45px" height="80px"></Image>,
-      amount: "325ml",
-      price: 6.5,
-    },
-    {
-      name: "Apple & Grape Juice",
-      productImage: <Image src={appleJuice} width="80px" height="80px"></Image>,
-      amount: "2L",
-      price: 15,
-    },
-    {
-      name: "Orange Juice",
-      productImage: (
-        <Image src={orangeJuice} width="80px" height="80px"></Image>
-      ),
-      amount: "2L",
-      price: 15,
-    },
-    {
-      name: "Coca Cola Can",
-      productImage: <Image src={coca} width="45px" height="80px"></Image>,
-      amount: "325ml",
-      price: 6.8,
-    },
-    {
-      name: "Pepsi Can",
-      productImage: <Image src={pepsi} width="45px" height="80px"></Image>,
-      amount: "330ml",
-      price: 6.8,
-    },
-  ];
+  // console.log(data)
+  const [displayedData, setDisplayedData] = useState([])
+  const [categorySet, setCategorySet] = useState(new Set())
+  const [brandSet, setBrandSet] = useState(new Set())
+
+  useEffect(()=>{
+    setDisplayedData(data)
+  })
+  // const [catList, setCatList] = useState([]);
+  // const [brandList, setBrandList] = useState([]);
+  // const beverageList = [
+  //   {
+  //     name: "Diet Coke",
+  //     productImage: <Image src={dietCoke} width="35px" height="80px"></Image>,
+  //     amount: "355ml",
+  //     price: 7,
+  //   },
+  //   {
+  //     name: "Sprite Can",
+  //     productImage: <Image src={sprite} width="45px" height="80px"></Image>,
+  //     amount: "325ml",
+  //     price: 6.5,
+  //   },
+  //   {
+  //     name: "Apple & Grape Juice",
+  //     productImage: <Image src={appleJuice} width="80px" height="80px"></Image>,
+  //     amount: "2L",
+  //     price: 15,
+  //   },
+  //   {
+  //     name: "Orange Juice",
+  //     productImage: (
+  //       <Image src={orangeJuice} width="80px" height="80px"></Image>
+  //     ),
+  //     amount: "2L",
+  //     price: 15,
+  //   },
+  //   {
+  //     name: "Coca Cola Can",
+  //     productImage: <Image src={coca} width="45px" height="80px"></Image>,
+  //     amount: "325ml",
+  //     price: 6.8,
+  //   },
+  //   {
+  //     name: "Pepsi Can",
+  //     productImage: <Image src={pepsi} width="45px" height="80px"></Image>,
+  //     amount: "330ml",
+  //     price: 6.8,
+  //   },
+  // ];
+  
+  // useEffect(() =>{
+  //   var categoryList = []
+  //   for (var i = 0; i < data.length; i++ ) {
+  //     for (var j = 0; j < data.length; j++ ) {
+  //       if (data[i].categoryTags[j] !== undefined) {
+  //         categoryList.push(data[i].categoryTags[j])
+  //       }
+  //     }
+  //   }
+  //   console.log(categoryList) 
+  // })
+
+  useEffect(() => {
+    var categories = new Set()
+    for (var i = 0; i < data.length; i++ ) {
+      for (var j = 0; j < data.length; j++ ) {
+        if (data[i].categoryTags[j] !== undefined) {
+          categories.add(data[i].categoryTags[j])
+        }
+      }
+    }    
+    setCategorySet(categories)
+  }, [])
+
+  useEffect(() => {
+    var brands = new Set()
+    for (var i = 0; i < data.length; i++ ) {
+      brands.add(data[i].brand)
+    }    
+    setBrandSet(brands)
+  }, [])
 
   return (
     <div className={moduleCss.container} style={{ overflow: showFilter ? "hidden" : "auto", height: showFilter ? "100vh" : "auto" }}>
@@ -90,12 +133,12 @@ const search = (props) => {
         {/* {beverageList.map((item, index) => (
           <Goods key={index}>{item}</Goods>
         ))} */}
-        {data.map((item, index) => (
+        {displayedData.map((item, index) => (
           <GoodsV2 key={index}>{item}</GoodsV2>
         ))}        
       </div>
       <NavBar />
-      <Filters onClose={() => setShowFilter(false)} show={showFilter}></Filters>
+      <Filters onClose={() => setShowFilter(false)} show={showFilter} categoryData={Array.from(categorySet)} brandData={Array.from(brandSet)}></Filters>
     </div>
   );
 };
@@ -103,6 +146,10 @@ const search = (props) => {
 export default search;
 
 export async function getServerSideProps(context) {
+  const authenticated = authenticationCheck(context)
+  if (!authenticated) {
+    return {redirect: {destination: '/', permanent: true,}, };
+  }
   const data = await fetch(`http://localhost:3000/api/search/${context.params.slug}`);
   const productData = await data.json();
 //   console.log("server side:" + productData)
