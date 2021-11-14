5b5ed3202673f066b3b54f91e346edd43cc7ec90
bennychengws <cwshing1@gmail.com>
2021-11-14 21:16:07 +0800

    added outputFileTracing: false

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     PAYPAL_CLIENT_ID: "AUNoSVgSLuz9QuQv9d414brbmTW5M4SKTgzbRe7MshLFjgOkoOtBGZG97zokiJDyHboqXjTrCu2rfwKr"
   },
   reactStrictMode: false,
+  outputFileTracing: false,
   publicRuntimeConfig: {
       apiUrl: process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000/api' // development api
