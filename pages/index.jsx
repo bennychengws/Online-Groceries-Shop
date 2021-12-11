ce5b0501dba1df0ce0d9c14981e5a62f5677c3d6
bennychengws <cwshing1@gmail.com>
2021-12-11 11:47:21 +0800

    Revert "started reorganizing css"
    This reverts commit f96a02749c152496f6cbd4391b7821a985eaba7c.

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,15 +13,15 @@ export default function Home() {
   return (
     <Layout>
       <div className={moduleCss.container}>
-        <div>
+        <div className={moduleCss.image}>
           <Image src={carrotImage} width="30vw" height="40vh"></Image>
         </div>
         <div className={moduleCss.loginContainer}>
-          <div>
-            <div>Login</div>
+          <div className={moduleCss.loginIntro}>
+            <div className={moduleCss.loggingIn}>Login</div>
             <div className={moduleCss.instructionContainer}>
-              <div>Enter your username and password</div>
-              <div><button onClick={() => setShowModal(true)}>Notice to Visitors</button></div>
+              <div className="text-gray-500">Enter your username and password</div>
+              <div className={moduleCss.noticeContainer}><button className={moduleCss.notice} onClick={() => setShowModal(true)}>Notice to Visitors</button></div>
             </div>
           </div>
           <Login />
